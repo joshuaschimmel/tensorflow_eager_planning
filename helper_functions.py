aaddf93053923566de49e529ca3ae480cb388c47
15schimme <5schimme@informati.uni-hamburg.de>
2019-02-19 15:07:47 +0100

    moved quality function to helper functions

--- helper_functions.py
+++ helper_functions.py
@@ -2,6 +2,8 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from matplotlib import pyplot as plt
+
 import pendulum as pend
 import forward_model_tf as fm
 
@@ -76,7 +78,9 @@ def get_plot_losses(predictions, realities):
         rmse_list.append(tf.sqrt(mse))
 
     # TODO learn to unpack this list
-    sim_cos, sim_sin, sim_dot = realities
+    sim_cos = []
+    sim_sin = []
+    sim_dot = []
     pred_cos = []
     pred_sin = []
     pred_dot = []
@@ -120,3 +124,91 @@ def plot_model_prediction(steps, model, model_name):
     # plot error functions (good for a single pass)
     plot_list = get_plot_losses(pred_states, sim_states)
     plot_graphs(title=model_name, plot_list=plot_list)
+
+
+def model_quality_analysis(test_runs: int,
+                           model: tf.keras.Model,
+                           steps: int,
+                           visualize: bool = True,
+                           plot_title: str = ""
+                           ):
+    """Calculates the mean rmse over multiple random instances.
+
+    Calculates the mean of the rmse values for test_runs number of
+    runs with steps number of steps. The calculated mean is the mean
+    over all test runs. If visualize is true, the rmse values and
+    the mean will be plotted against each other in a single plot.
+
+    :param test_runs: # of random tries
+    :param model: the model to be tested
+    :param steps: # of steps in a single try
+    :param visualize: whether the result should be visualized
+    :param plot_title: the title of the plot
+    :return:
+    """
+    all_rmse_values = []
+    plot_list = []
+    for i in range(test_runs):
+        # create a new random plan
+        plan = get_random_plan(steps)
+
+        # let the simulation run on the plan to create
+        # the expected states as well as the starting state
+        # s_0
+        sim_states = pend.run_simulation_plan(plan=plan)
+        # get starting state
+        s_0 = sim_states[0]
+        # let the model predict the states
+        pred_states = fm.predict_states(model=model, state_0=s_0, plan=plan)
+
+        current_rmse_values = []
+        # calculate the rmse
+        for pred_state, sim_state in zip(pred_states, sim_states):
+            current_rmse_values.append(
+                fm.singleton_rmse_loss(prediction=pred_state,
+                                       target=sim_state
+                                       )
+            )
+        # append the values to the list of values as an numpy array
+        all_rmse_values.append(np.array(current_rmse_values))
+        plot_list.append({
+            "label": i+1,
+            "values": np.array(current_rmse_values),
+            "format": "-"
+        })
+
+    mean_array = np.mean(all_rmse_values, axis=0)
+    mean_dict = {
+        "label": "Mean",
+        "values": mean_array,
+        "format": ""
+    }
+
+    if visualize:
+        # initialize figure
+        plt.figure()
+        # plot title
+        plt.suptitle(plot_title)
+        # iterate through list and plot all entries
+        for plot_data in plot_list:
+            plt.plot(plot_data["values"],
+                     plot_data["format"],
+                     label=plot_data["label"],
+                     linewidth=1,
+                     alpha=0.5
+                     )
+        # plot the mean
+        mean_plot = plt.plot(mean_dict["values"],
+                             mean_dict["format"],
+                             label=mean_dict["label"],
+                             linewidth=2
+                             )
+
+
+        plt.ylim(0, 10)
+        plt.grid(b=True, alpha=0.25, linestyle="--")
+        plt.tick_params(axis="both", which="major", direction="out")
+        plt.legend(mean_plot, ["Mean"], loc=1)
+        plt.show()
+
+    return np.add.reduce(mean_array)
--- trainer.py
+++ trainer.py
@@ -1,11 +1,6 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
 import forward_model_tf as fm
-import pendulum as pend
-import helper_functions as _hf
-
+import helper_functions as hf
 
 tf.enable_eager_execution()
 
@@ -60,93 +55,11 @@ if not _load_model:
     fm.save_model(model, model_path)
     print("saved")
 
-def model_quality_analysis(test_runs: int,
-                           model: tf.keras.Model,
-                           steps: int,
-                           visualize: bool = True,
-                           plot_title: str = ""
-                           ):
-    """Calculates the mean rmse over multiple random instances.
-
-    Calculates the mean of the rmse values for test_runs number of
-    runs with steps number of steps. The calculated mean is the mean
-    over all test runs. If visualize is true, the rmse values and
-    the mean will be plotted against each other in a single plot.
-
-    :param test_runs: # of random tries
-    :param model: the model to be tested
-    :param steps: # of steps in a single try
-    :param visualize: whether the result should be visualized
-    :param plot_title: the title of the plot
-    :return:
-    """
-    all_rmse_values = []
-    plot_list = []
-    for i in range(test_runs):
-        # create a new random plan
-        plan = _hf.get_random_plan(steps)
-
-        # let the simulation run on the plan to create
-        # the expected states as well as the starting state
-        # s_0
-        sim_states = pend.run_simulation_plan(plan=plan)
-        # get starting state
-        s_0 = sim_states[0]
-        # let the model predict the states
-        pred_states = fm.predict_states(model=model, state_0=s_0, plan=plan)
-
-        current_rmse_values = []
-        # calculate the rmse
-        for pred_state, sim_state in zip(pred_states, sim_states):
-            current_rmse_values.append(
-                fm.singleton_rmse_loss(prediction=pred_state,
-                                       target=sim_state
-                                       )
-            )
-        # append the values to the list of values as an numpy array
-        all_rmse_values.append(np.array(current_rmse_values))
-        plot_list.append({
-            "label": i+1,
-            "values": np.array(current_rmse_values),
-            "format": "-"
-        })
-
-    mean_dict = {
-        "label": "Mean",
-        "values": np.mean(all_rmse_values, axis=0),
-        "format": ""
-    }
-
-    if visualize:
-        # initialize figure
-        plt.figure()
-        # plot title
-        plt.suptitle(plot_title)
-        # iterate through list and plot all entries
-        for plot_data in plot_list:
-            plt.plot(plot_data["values"],
-                     plot_data["format"],
-                     label=plot_data["label"],
-                     linewidth=1,
-                     alpha=0.5
-                     )
-        # plot the mean
-        mean_plot = plt.plot(mean_dict["values"],
-                             mean_dict["format"],
-                             label=mean_dict["label"],
-                             linewidth=2
-                             )
-
-
-        plt.ylim(0, 10)
-        plt.grid(b=True, alpha=0.25, linestyle="--")
-        plt.tick_params(axis="both", which="major", direction="out")
-        plt.legend(mean_plot, ["Mean"], loc=1)
-        plt.show()
+hf.plot_model_prediction(200, model, model_name)
 
 plot_title = f"RMSE for {_test_runs} Random Initializations"
-model_quality_analysis(test_runs=_test_runs,
-                       model=model,
-                       steps=_steps,
-                       visualize=True,
-                       plot_title=plot_title)
+print(hf.model_quality_analysis(test_runs=_test_runs,
+                                model=model,
+                                steps=_steps,
+                                visualize=True,
+                                plot_title=plot_title))
